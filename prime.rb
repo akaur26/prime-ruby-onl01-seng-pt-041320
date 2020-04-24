a98e507a7215eb946f962ce7d5ecaf03a2f2a232
Anmol Kaur <kaur.anmol26@gmail.com>
2020-04-24 17:59:12 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,40 +1,40 @@
-def prime?(number)
-  n = 1
-   if n > 2
-     true
-   elsif
-     n == 3
-   else
-    n <= 2
-   end
-   prime?
-end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 # def prime?(number)
-#   start = 2
-#   if number > 1
-#     range = (start..number-1).to_a
-#     range.none? do |num_to_test| 
-#       number % num_to_test == 0
-#     end
+#   n = 1
+#   if n > 2
+#     true
+#   elsif
+#     n == 3
 #   else
-#     false
+#     n <= 2
 #   end
+#   prime?
 # end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def prime?(number)
+  start = 2
+  if number > 1
+    range = (start..number-1).to_a
+    range.none? do |num_to_test| 
+      number % num_to_test == 0
+    end
+  else
+    false
+  end
+end
